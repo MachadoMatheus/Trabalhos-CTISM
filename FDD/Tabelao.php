c36cd97ecf4e8ff8148594a6a320b193833a0692
Matheus Machado <matheusmazar@yahoo.com.br>
2019-06-22 08:25:50 -0300

    Update Diagrama.png, FDD - Img - Exemplo Prático.zip, and 7 more files...

---
+++ FDD/Tabelao.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Tabelão</title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+	<div class="fundo">
+		<table style="float: left;width: 25%">
+			<tr>
+				<td style="height: 120px; background-color: yellow; padding-bottom: 0px;">Feature: 001<br>Módulo de Segurança<br><br><br><br><br>72%</td>
+			</tr>
+			<tr>
+				<td>
+					<div style="float: left; background-color: green; height: 20px;  width: 72%;"></div>
+				</td>
+			</tr>
+			<tr style="padding: none;">
+				<td>12/01/2019</td>
+			</tr>
+		</table>
+
+
+
+		<table style="float: left;width: 25%">
+			<tr>
+				<td style="height: 120px; background-color: red; padding-bottom: 0px;">Feature: 002<br>Consulta de Matéria Prima<br><br><br><br><br>27%</td>
+			</tr>
+			<tr>
+				<td>
+					<div style="float: left; background-color: green; height: 20px;  width: 27%;"></div>
+				</td>
+			</tr>
+			<tr style="padding: none;">
+				<td>12/01/2019</td>
+			</tr>
+		</table>
+
+
+
+		<table style="float: left;width: 25%">
+			<tr>
+				<td style="height: 120px; background-color: green; padding-bottom: 0px;">Feature: 003<br>Cadastro de Usuário<br><br><br><br><br>100%</td>
+			</tr>
+			<tr>
+				<td>
+					<div style="float: left; background-color: green; height: 20px;  width: 100%;"></div>
+				</td>
+			</tr>
+			<tr style="padding: none;">
+				<td>12/01/2019</td>
+			</tr>
+		</table>
+
+
+
+		<table style="float: left;width: 25%">
+			<tr>
+				<td style="height: 120px; background-color: green; padding-bottom: 0px;">Feature: 004<br>Cadastro de Distribuídores<br><br><br><br><br>100%</td>
+			</tr>
+			<tr>
+				<td>
+					<div style="float: left; background-color: green; height: 20px;  width: 100%;"></div>
+				</td>
+			</tr>
+			<tr style="padding: none;">
+				<td>12/01/2019</td>
+			</tr>
+		</table>
+
+
+
+		<table style="float: left;width: 25%">
+			<tr>
+				<td style="height: 120px; background-color: yellow; padding-bottom: 0px;">Feature: 005<br>Cadastro de Setores<br><br><br><br><br>19%</td>
+			</tr>
+			<tr>
+				<td>
+					<div style="float: left; background-color: green; height: 20px;  width: 19%;"></div>
+				</td>
+			</tr>
+			<tr style="padding: none;">
+				<td>14/04/2019</td>
+			</tr>
+		</table>
+
+
+
+		<table style="float: left;width: 25%">
+			<tr>
+				<td style="height: 120px; background-color: yellow; padding-bottom: 0px;">Feature: 006<br>Consulta de Distribuidores<br><br><br><br><br>80%</td>
+			</tr>
+			<tr>
+				<td>
+					<div style="float: left; background-color: green; height: 20px;  width: 80%;"></div>
+				</td>
+			</tr>
+			<tr style="padding: none;">
+				<td>14/04/2019</td>
+			</tr>
+		</table>
+
+
+
+		<table style="float: left;width: 25%">
+			<tr>
+				<td style="height: 120px; background-color: white; padding-bottom: 0px;">Feature: 007<br>Controle de Estoque<br><br><br><br><br>0%</td>
+			</tr>
+			<tr>
+				<td>
+					<div style="float: left; background-color: green; height: 20px;  width: 0%;"></div>
+				</td>
+			</tr>
+			<tr style="padding: none;">
+				<td>27/11/2019</td>
+			</tr>
+		</table>
+
+
+
+		<table style="float: left;width: 25%">
+			<tr>
+				<td style="height: 120px; background-color: white; padding-bottom: 0px;">Feature: 008<br>Relatório de Estoque Máximo<br><br><br><br>0%</td>
+			</tr>
+			<tr>
+				<td>
+					<div style="float: left; background-color: green; height: 20px;  width: 0%;"></div>
+				</td>
+			</tr>
+			<tr style="padding: none;">
+				<td>27/11/2019</td>
+			</tr>
+		</table>
+
+	</div>
+</body>
+</html>
